ccc53c37b91b1cf9ffed449b718b078209fffa8f
khkim <khkim@gaia3d.com>
2021-12-29 11:20:59 +0900

    init

---
+++ src/main/java/com/example/vue_with_boot_same_port/VueWithBootSamePortApplication.java
@@ -0,0 +1,13 @@
+package com.example.vue_with_boot_same_port;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueWithBootSamePortApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueWithBootSamePortApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vue_with_boot_same_port/VueWithBootSamePortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vue_with_boot_same_port;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueWithBootSamePortApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
